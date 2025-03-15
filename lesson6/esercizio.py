edf9ddf897f834a9637fe25173bdfa6c3c6bd4a3
hiroki <hiroki83inoue@icloud.com>
2025-03-15 11:35:13 +0100

    fish excercise for lesson6

--- lesson6/esercizio.py
+++ lesson6/esercizio.py
@@ -5,26 +5,6 @@ Created on Wed Mar 12 08:51:41 2025
 
 @author: hirokiinoue
 
-2. countw(t, w) ritorna il numero di occorrenze della parola w nella stringa t . 
-	Esempio
-	t = 'le cose non sono solo cose, ma anche cosette'
-	countw(t, 'cose') 		ritorna		2
-
-3. digits(t) ritorna la lista delle sequenze numeriche contenute nella stringa t . 
-   Per sequenza numerica intendiamo una sequenza di cifre (caratteri 0 , 1 ,…, 9 ) di lunghezza massimale. 
-	Esempio
-	t = 'via Po n.23, tel. 06 7867555 - cell. 345 675665676 (cc 34565)'
-	digits(t) 			ritorna 	['23', '06', '7867555', '345', '675665676', '34565']
-
-4. column(table, k) ritorna una lista che contiene i valori della colonna k della tabella table . 
-   La tabella è rappresentata in modo che ogni linea di table contiene una riga e i valori delle colonne sono separati
-   dal carattere ';' . Se table ha n colonne, allora ogni linea di table conterrà esattamente n-1 caratteri ';' .
-	Esempio
-	table = '''Milano;12;23
-Roma;16;25
-Napoli;15;27
-Firenze;11;20'''
-	column(table, 1)		ritorna		['12', '16', '15', '11']
 
 ##########################################
 
@@ -60,5 +40,89 @@ Chi vuol esser lieto, sia:
 del doman non c’è certezza.'''
 	firstline(t, 'non')		ritorna		'del doman non c’è certezza.'
 """
-def firstline(t s):
-    text = splitline(t)
+def firstline(t, s):
+    text = t.splitlines()
+    print(text)
+    for line in text:
+        if s in line:
+            return line
+    return None
+
+"""
+
+2. countw(t, w) ritorna il numero di occorrenze della parola w nella stringa t . 
+	Esempio
+	t = 'le cose non sono solo cose, ma anche cosette'
+	countw(t, 'cose') 		ritorna		2
+"""
+def countw(t, w):
+    cnt = 0
+    words = t.split()
+    for word in words:
+        alpha_italian = 'abcdefghijklmnopqrstuvwxyzàèéìòù'
+        charAry = list(word)
+        for char in charAry:
+            if not char in alpha_italian:
+                """print(char,word)"""
+                word = word.replace(char,'')
+                """print(char,word)"""
+        if w in word and len(w) == len(word):
+            cnt += 1
+    return cnt
+
+def countwv2(t, w):
+    text = t.split()
+    return text.count(w)
+
+def countwProf(t, w):
+    non_alfa = []
+    for c in t:
+        if c not in non_alfa and not c.isalpha():
+            non_alfa.append(c)
+    for c in non_alfa:
+        t = t.replace(c, ' ')
+        parole = t.split()
+        print(parole)
+    return parole.count(w)
+
+"""
+3. digits(t) ritorna la lista delle sequenze numeriche contenute nella stringa t . 
+   Per sequenza numerica intendiamo una sequenza di cifre (caratteri 0 , 1 ,…, 9 ) di lunghezza massimale. 
+	Esempio
+	t = 'via Po n.23, tel. 06 7867555 - cell. 345 675665676 (cc 34565)'
+	digits(t) 			ritorna 	['23', '06', '7867555', '345', '675665676', '34565']
+
+"""
+def digits(t):
+    for i in range(len(t)):
+        if not t[i].isnumeric():
+            t = t.replace(t[i], ' ')
+    words = t.split()
+    rtn = []
+    for word in words:
+        if len(word) > 1:
+            rtn += [word]
+    return rtn
+
+"""
+4. column(table, k) ritorna una lista che contiene i valori della colonna k della tabella table . 
+   La tabella è rappresentata in modo che ogni linea di table contiene una riga e i valori delle colonne sono separati
+   dal carattere ';' . Se table ha n colonne, allora ogni linea di table conterrà esattamente n-1 caratteri ';' .
+	Esempio
+	table = '''Milano;12;23
+Roma;16;25
+Napoli;15;27
+Firenze;11;20'''
+	column(table, 1)		ritorna		['12', '16', '15', '11']
+
+"""
+def column(table, k):
+    rtn = []
+    lines = table.splitlines()
+    for line in lines:
+        columns = line.split(';')
+        if len(columns) <= k:
+            rtn = "the value of second parameter is greater than the number of table columns!"
+            break
+        rtn += [columns[k]]
+    return rtn
