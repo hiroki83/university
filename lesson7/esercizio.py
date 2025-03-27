349f7db3e432e2e9844a1bdd7944743713536fef
hiroki <hiroki83inoue@icloud.com>
2025-03-27 09:01:25 +0100

    WIP exercise7

--- lesson7/esercizio.py
+++ lesson7/esercizio.py
@@ -42,3 +42,14 @@ Usando la list comprehension definite le seguenti funzioni:
 
 ################################################################################
 '''
+def triangolo(n):
+    rtn = []
+    for i in range(1,n+1):
+        tmp = []
+        for j in range(1,i+1):
+            tmp += [i*j]
+        rtn += [tmp]
+    return rtn
+
+def triangoloWithComprehension(n):
+    return None
